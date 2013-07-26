9ddba6b27fe5fe0ecd77d5ea1d6c4ba9e7d16342
SoullessWaffle <mickeydekkers@gmail.com>
2013-07-26 12:30:31 +0200

    Did lots of stuff
    Added not-admin message and condition, merged friendMsg and messsage
    listeners, moved easterEggs, fixed grammar, added logging function (for
    later addition of timestamps), added optional accountData output to log,
    moved setpersonastate to on('relationships') to ensure that bot is
    prepared when it goes online, added logging to basically everything and
    prettified it using jsbeautifier.org

--- steamBot.js
+++ steamBot.js
@@ -1,8 +1,6 @@
-
 /**************************************
  *          String Constants          *
  **************************************/
- 
 var MSG_INVALID_COMMAND = "You have entered an invalid command. Please try again.";
 
 var MSG_SUCCESSFUL_OP = "Added ID to admins!";
@@ -11,11 +9,18 @@ var MSG_ALREADY_OP = "That ID is already set to admin.";
 var MSG_SUCCESSFUL_DEOP = "Removed ID from admins!";
 var MSG_ALREADY_DEOP = "That ID is already not set to admin.";
 
+var MSG_NOT_ADMIN = "You are not an admin!";
+
 var MSG_OPS_LIST_PREFIX = "Admins:\n";
 
 var MSG_SUCCESSFUL_TOGGLE = "Value toggled!";
 var MSG_TOGGLE_LIST_PREFIX = "Toggleables:\n";
 
+var easterEggs = {
+    'ping': 'pong',
+    'poop': 'piss',
+    'dog': 'cat'
+};
 
 
 
@@ -24,7 +29,7 @@ var MSG_TOGGLE_LIST_PREFIX = "Toggleables:\n";
  *********************************/
 
 /** Emulated Typing mode */
-var emutyping_enabled = true; // if true, bot will seem to be typing before it sends a message
+var emutyping_enabled = false; // if true, bot will seem to be typing before it sends a message
 var emutyping_speed = 20; // typing speed (in characters per second)
 var emutyping_maxdelay = 3; // maximum delay (in seconds) before the message will finally be sent
 
@@ -45,120 +50,126 @@ var steamTrade = new SteamTrade();
 var fs = require('fs');
 
 /** 
- * Convenience function for send chat messages to the user
+ * Convenience function for sending chat messages to the user
  */
+
 function typeMessage(steamID, message) {
-    if (emutyping_enabled) {
+    if(emutyping_enabled) {
         var delay = Math.min(message.length * (1000 / emutyping_speed), emutyping_maxdelay * 1000);
-        
+
         bot.sendMessage(steamID, "", Steam.EChatEntryType.Typing);
-        setTimeout((function() {
+        setTimeout((function () {
             bot.sendMessage(steamID, message, Steam.EChatEntryType.ChatMsg);
         }), delay);
-        
+
     } else {
         bot.sendMessage(steamID, message, Steam.EChatEntryType.ChatMsg);
     }
 }
 
+/** 
+ * Convenience function for logging
+ */
 
-fs.exists('accountData.txt', function(exists) {
-    if (exists){
+function log(msg) {
+    console.log(msg);
+}
+
+fs.exists('accountData.txt', function (exists) {
+    if(exists) {
         var accountData = fs.readFileSync('accountData.txt').toString().split('\n');
-        console.log('Loaded account details from file');
+        log('Loaded account details from file');
+        //log('User: '+accountData[0]+' Pass: '+accountData[1]); //print accountData to console for debugging purposes
         bot.logOn({
             accountName: accountData[0],
             password: accountData[1]
         });
-        
-        
-        bot.on('loggedOn', function() {
-            console.log('Logged in as ' + bot.steamID);
-            bot.setPersonaState(Steam.EPersonaState.Online);
+
+
+        bot.on('loggedOn', function () {
+            log('Logged in as ' + bot.steamID);
+            //moved personastate to relationships to ensure bot is ready for everything when it visibly comes online
             //bot.setPersonaName('Coupon Bot');
         });
-        
-        
-        bot.on('relationships', function() {
-            console.log('Relationships loaded');
-            /*console.log('Steam friends list: '+String(Steam.friends));
+
+
+        bot.on('relationships', function () {
+            log('Relationships loaded');
+            bot.setPersonaState(Steam.EPersonaState.Online);
+            /*log('Steam friends list: '+String(Steam.friends));
             Steam.friends && Steam.friends.forEach(function(steamID) {
-                console.log(String(steamID));
+                log(String(steamID));
             });*/
-            //bot.addFriend('STEAM_0:1:24409951');
         });
-        
-        
-        bot.on('chatInvite', function(chatRoomID, chatRoomName, patronID) {
-            console.log('Got an invite to ' + chatRoomName + ' from ' + bot.users[patronID].playerName);
+
+
+        bot.on('chatInvite', function (chatRoomID, chatRoomName, patronID) {
+            log('Got an invite to ' + chatRoomName + ' from ' + bot.users[patronID].playerName);
             bot.joinChat(chatRoomID); // autojoin on invite
         });
-        
-        
-        var easterEggs = {'ping':'pong','poop':'piss','dog':'cat'};
-        bot.on('message', function(uid, msg, type, chatter) {
-            // respond to both chat room and private messages
-            if (msg != '') {
-                console.log('Received message: ' + msg);
-                if (msg in easterEggs) {
-                    typeMessage(uid, easterEggs[msg]);
-                }
-                
-            }
-        });
-        
-        
-        bot.on('friendMsg', function(uid, msg, msgtype) {
-            if (msgtype == Steam.EChatEntryType.ChatMsg) {
-                
+
+
+
+        //Merged friendMessage and message into message so admins can command bot in chat room
+        bot.on('message', function (uid, msg, msgtype) {
+            if(msgtype == Steam.EChatEntryType.ChatMsg) {
+
                 /** if the user is sending an admin command and has permission to do so */
-                if (msg.indexOf("/") == 0 && adminIDs.indexOf(uid) > -1) {
+                if(msg.indexOf("/") == 0 && adminIDs.indexOf(uid) > -1) {
                     var cmd = msg.substr(1);
                     var cmdparams = cmd.split(" ");
-                    
+
                     /** "/op <steamID64>" to add someone to the admin list */
-                    if (cmdparams[0].toLowerCase() == "op") {
-                        if (cmdparams.length == 2) {
+                    if(cmdparams[0].toLowerCase() == "op") {
+                        if(cmdparams.length == 2) {
                             //should probably be checking if they actually entered a valid steam ID
                             //should be saving adminIDs to a file so they survive reboots
-                            if (adminIDs.indexOf(cmdparams[1]) > -1) {
+                            if(adminIDs.indexOf(cmdparams[1]) > -1) {
                                 typeMessage(uid, MSG_ALREADY_OP);
+                                log('Admin with SteamID ' + uid + ' tried to OP SteamID ' + cmdparams[1] + ' but that SteamID is already an admin!');
                             } else {
                                 adminIDs.push(cmdparams[1]);
                                 typeMessage(uid, MSG_SUCCESSFUL_OP);
+                                log('Admin with SteamID ' + uid + ' successfully OP\'d SteamID ' + cmdparams[1]);
                             }
                         } else {
                             typeMessage(uid, MSG_INVALID_COMMAND);
+                            log('Admin with SteamID ' + uid + ' entered invalid command: ' + msg);
                         }
-                        
-                    /** "/deop <steamID64>" to remove someone from the admin list */
-                    } else if (cmdparams[0].toLowerCase() == "deop") {
-                        if (cmdparams.length == 2) {
+
+                        /** "/deop <steamID64>" to remove someone from the admin list */
+                    } else if(cmdparams[0].toLowerCase() == "deop") {
+                        if(cmdparams.length == 2) {
                             //should probably be checking if they actually entered a valid steam ID
                             //should be saving adminIDs to a file so they survive reboots
                             var index = adminIDs.indexOf(cmdparams[1])
-                            if (index == -1) {
+                            if(index == -1) {
                                 typeMessage(uid, MSG_ALREADY_DEOP);
+                                log('Admin with SteamID ' + uid + ' tried to DEOP SteamID ' + cmdparams[1] + ' but that SteamID is already DEOP\'d!');
                             } else {
-                                adminIDs = adminIDs.slice(0, index).concat(adminIDs.slice(index+1));
+                                adminIDs = adminIDs.slice(0, index).concat(adminIDs.slice(index + 1));
                                 typeMessage(uid, MSG_SUCCESSFUL_DEOP);
+                                log('Admin with SteamID ' + uid + ' successfully DEOP\'d SteamID ' + cmdparams[1]);
                             }
                         } else {
                             typeMessage(uid, MSG_INVALID_COMMAND);
+                            log('Admin with SteamID ' + uid + ' entered invalid command: ' + msg);
                         }
-                        
-                    /** "/ops" to list all current admins */
-                    } else if (cmdparams[0].toLowerCase() == "ops") {
-                        if (cmdparams.length == 1) {
+
+                        /** "/ops" to list all current admins */
+                    } else if(cmdparams[0].toLowerCase() == "ops") {
+                        if(cmdparams.length == 1) {
                             typeMessage(uid, MSG_OPS_LIST_PREFIX + adminIDs.join("\n"));
+                            log('Admin with SteamID ' + uid + ' requested the list of OPs');
                         } else {
                             typeMessage(uid, MSG_INVALID_COMMAND);
+                            log('Admin with SteamID ' + uid + ' entered invalid command: ' + msg);
                         }
-                    
-                    /** THIS METHOD IS BROKEN RIGHT NOW BUT IT'S 2 AM AND I HAVE WORK
+
+                        /** THIS METHOD IS BROKEN RIGHT NOW BUT IT'S 2 AM AND I HAVE WORK
                        TOMORROW MORNING SO IT'S STAYING BROKEN AND COMMENTED FOR NOW */
-                    /** "/toggle <varname>" to enable/disable a feature or whatever */
-                    /*} else if (cmdparams[0].toLowerCase() == "toggle") {
+                        /** "/toggle <varname>" to enable/disable a feature or whatever */
+                        /*} else if (cmdparams[0].toLowerCase() == "toggle") {
                         var toggleables = [
                             ["emutyping", "emutyping_enabled"]
                         ];
@@ -191,29 +202,38 @@ fs.exists('accountData.txt', function(exists) {
                         } else {
                             typeMessage(uid, MSG_INVALID_COMMAND);
                         }
-                      */  
+                      */
                     } else {
                         typeMessage(uid, MSG_INVALID_COMMAND);
+                        log('Admin with SteamID ' + uid + ' entered invalid command: ' + msg);
                     }
                 } else {
-                    //code to run when the message is not an admin command
+                    if(msg.indexOf("/") == 0 && adminIDs.indexOf(uid) == -1) {
+                        typeMessage(uid, MSG_NOT_ADMIN);
+                        log('Non-admin user with SteamID ' + uid + ' tried to execute command: ' + msg);
+                    } else {
+                        if(msg in easterEggs) {
+                            log('Received easter egg: ' + msg + 'Replying with: ' + easterEggs[msg]);
+                            typeMessage(uid, easterEggs[msg]);
+                        } else {
+                            log('Received message from SteamID ' + uid + ': ' + msg);
+                        }
+                    }
                 }
             }
         });
-        
-        
-        bot.on('friend', function(steamID, relationshipStatus) {
-            //console.log('Friend Activity!');
-            if (relationshipStatus == Steam.EFriendRelationship['RequestRecipient']){
-                //console.log('Pending friend request!');
+
+
+        bot.on('friend', function (steamID, relationshipStatus) {
+            //log('Friend Activity!');
+            if(relationshipStatus == Steam.EFriendRelationship['RequestRecipient']) {
+                //log('Pending friend request!');
                 bot.addFriend(steamID);
-                console.log('Added '+String(steamID)+' to friends list');
+                log('Added ' + String(steamID) + ' to friends list');
             }
         });
-    }
-    else{
-        console.log('Could not load account details, shutting down...');
+    } else {
+        log('Could not load account details, shutting down...');
         process.exit();
     }
 });
-
